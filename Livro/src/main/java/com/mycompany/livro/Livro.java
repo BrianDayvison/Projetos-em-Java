477d29b3e8b0e7112a2cc6b28e8b17ea66731dd9
BrianDayvison <106290452+BrianDayvison@users.noreply.github.com>
2022-11-23 18:58:51 -0300

    Primeira fase
    Foi criado tudo que é referente a pessoa, assim como esta no diagrama.

---
+++ Livro/src/main/java/com/mycompany/livro/Livro.java
@@ -0,0 +1,12 @@
+package com.mycompany.livro;
+public class Livro 
+{
+
+    public static void main(String[] args) 
+    {
+        Pessoa P[] = new Pessoa[3];
+        P[1] = new Pessoa ("Brian",26,"Masculino");
+        P[1].dados();
+        
+    }
+}
---
+++ Livro/src/main/java/com/mycompany/livro/Pessoa.java
@@ -0,0 +1,60 @@
+package com.mycompany.livro;
+public class Pessoa 
+{
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    public Pessoa(String no, int id, String sx) 
+    {
+        this.nome = no;
+        this.idade = id;
+        this.sexo = sx;
+    }
+    
+    
+    
+    
+    //-----------------------//
+    public String getNome() 
+    {
+        return nome;
+    }
+
+    public void setNome(String no) 
+    {
+        this.nome = no;
+    }
+
+    public int getIdade() 
+    {
+        return idade;
+    }
+
+    public void setIdade(int id) 
+    {
+        this.idade = id;
+    }
+
+    public String getSexo() 
+    {
+        return sexo;
+    }
+
+    public void setSexo(String sx) 
+    {
+        this.sexo = sx;
+    }
+    
+    //------------------------------//
+    
+    public void fazerAniversario()
+    {
+        this.setIdade(this.getIdade() + 1);
+    }
+    
+    public void dados()
+    {
+        System.out.println("Nome: " + this.getNome() + "\nIdade: " + this.getIdade() + "\nSexo: " + this.getSexo());
+    }
+}
