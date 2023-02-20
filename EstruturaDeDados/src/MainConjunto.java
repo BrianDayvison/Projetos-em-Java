b7487906951aaa56f4e52b43be3ee24e33716764
Brian Dayvison Soares de Souza <briandayvison020@gmail.com>
2023-02-20 14:28:23 -0300

    Estrutura de Dados
    Utilizando Conjuntos

---
+++ EstruturaDeDados/src/MainConjunto.java
@@ -0,0 +1,32 @@
+import classes.Conjunto;
+import java.util.LinkedList;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author brian
+ */
+public class MainConjunto {
+
+    public static void main(String[] args) {
+        
+       Conjunto conjunto = new Conjunto();
+        conjunto.adiciona("Andreia");
+        conjunto.adiciona("Anderson");
+        System.out.println(conjunto);
+
+        conjunto.adiciona("Mauricio");
+        System.out.println(conjunto);
+        
+        conjunto.remove("Mauricio");
+        System.out.println(conjunto);
+    }
+}
+        
+       
+    
+    
+
---
+++ EstruturaDeDados/src/classes/Conjunto.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author brian
+ */
+public class Conjunto{
+    
+    private ArrayList<LinkedList<String>> tabela = new ArrayList<LinkedList<String>>();
+    
+    public Conjunto() {
+        for(int i = 0; i < 26; i++) {
+            tabela.add(new LinkedList<String>());
+        }
+    }
+    
+    private int calculaIndiceDaTabela(String palavra) {
+        return palavra.toLowerCase().charAt(0) % 26;
+    }
+    
+    private boolean contem(String palavra) {
+        int indice = calculaIndiceDaTabela(palavra);
+        return tabela.get(indice).contains(palavra);
+    }
+    
+    public void adiciona(String palavra) {
+        if(!contem(palavra)) {
+            int indice = calculaIndiceDaTabela(palavra);
+            List<String> lista = tabela.get(indice);
+            lista.add(palavra);
+        }
+    }
+    
+    public void remove(String palavra) {
+        if(contem(palavra)) {
+        int indice = calculaIndiceDaTabela(palavra);
+        List<String> lista = tabela.get(indice);
+        lista.remove(palavra);
+        }
+    }
+    @Override
+    public String toString() {
+        return tabela.toString();
+    }
+}
+
